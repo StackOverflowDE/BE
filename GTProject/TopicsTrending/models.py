756e6769098b88d043b054b4306425e0d4f86540
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-04-16 11:14:48 +0900

    db create
    + Skill, Repository, Question, TechBlog, Job 테이블

--- GTProject/TopicsTrending/models.py
+++ GTProject/TopicsTrending/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    repository = models.ForeignKey('Repository', on_delete=models.CASCADE, null=False)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, null=False)
+    techBlog = models.ForeignKey('TechBlog', on_delete=models.CASCADE, null=False)
+
+class Repository(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    url = models.CharField(max_length=100, null=False)
+
+class Question(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    url = models.CharField(max_length=100, null=False)
+
+class TechBlog(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    url = models.CharField(max_length=100, null=False)
+
+class Job(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, null=False)
