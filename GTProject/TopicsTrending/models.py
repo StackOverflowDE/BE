c54aa36d095d54d42fccbf2af667c58d65901346
culonculon <culonculon48@gmail.com>
2024-04-16 19:32:54 +0900

    test_data.txt를 활용해서 DB에 적재시키는 모듈 생성 (수정1)

--- GTProject/TopicsTrending/models.py
+++ GTProject/TopicsTrending/models.py
@@ -16,6 +16,22 @@ class Repository(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @classmethod
+    def add_data_from_file(cls, file_path):
+        # 텍스트 파일 열기
+        with open(file_path, 'r', encoding='utf-8') as file:
+            # 각 줄을 읽어와 데이터베이스에 추가
+            for line in file:
+                # 줄을 쉼표로 분할하여 필드 값 가져오기
+                fields = line.strip().split(',')
+                # 데이터베이스에 추가
+                cls.objects.create(
+                    title=fields[0],
+                    url=fields[1],
+                    created_at=timezone.datetime.strptime(fields[2], '%Y-%m-%d %H:%M:%S'),
+                    updated_at=timezone.datetime.strptime(fields[3], '%Y-%m-%d %H:%M:%S')
+                )
+
 
 
 class Question(models.Model):
--- GTProject/subModule.py
+++ GTProject/subModule.py
@@ -1,21 +1,10 @@
 import os
-from django.utils import timezone
-
-def add_data_from_file(cls, file_path):
-    # 텍스트 파일 열기
-    with open(file_path, 'r', encoding='utf-8') as file:
-        # 각 줄을 읽어와 데이터베이스에 추가
-        for line in file:
-            # 줄을 쉼표로 분할하여 필드 값 가져오기
-            fields = line.strip().split(',')
-            # 데이터베이스에 추가
-            cls.objects.create(
-                title=fields[0],
-                url=fields[1],
-                created_at=timezone.datetime.strptime(fields[2], '%Y-%m-%d %H:%M:%S'),
-                updated_at=timezone.datetime.strptime(fields[3], '%Y-%m-%d %H:%M:%S')
-            )
+from TopicsTrending.models import Repository
 
 current_directory = os.path.dirname(os.path.abspath(__name__))
 file_path = os.path.join(current_directory, 'test_data.txt')
-add_data_from_file(file_path)
+Repository.add_data_from_file(file_path)
+
+# python manage.py shell 에서 사용된다.
+# exec(open("./subModule.py").read())
+
