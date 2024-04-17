8a82d89fc3ce413852de99755805d8dbc7f0995f
이승준 <127376832+hhee4455@users.noreply.github.com>
2024-04-17 12:34:22 +0900

    Skill tabel count 수정중
    + name + job 같을시 count만 업데이트

--- GTProject/TopicsTrending/models.py
+++ GTProject/TopicsTrending/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from django.utils import timezone
 
-class Skill(models.Model):
-    name = models.CharField(max_length=100, null=False)
+class Job(models.Model):
+    name = models.CharField(max_length=100, default='', null=False)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    job = models.ForeignKey('Job', on_delete=models.CASCADE, null=True)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:  # Check if the Skill object already exists
+            # If it exists, update the count directly
+            existing_skill = Skill.objects.filter(name=self.name, job=self.job).first()
+            if existing_skill:
+                existing_skill.count += 1
+                existing_skill.save(update_fields=['count'])
+        else:
+            super().save(*args, **kwargs)
 
 
 class Repository(models.Model):
@@ -24,13 +40,9 @@ class Repository(models.Model):
 
     @classmethod
     def add_data_from_file(cls, file_path):
-        # 텍스트 파일 열기
         with open(file_path, 'r', encoding='utf-8') as file:
-            # 각 줄을 읽어와 데이터베이스에 추가
             for line in file:
-                # 줄을 쉼표로 분할하여 필드 값 가져오기
                 fields = line.strip().split(',')
-                # 데이터베이스에 추가
                 cls.objects.create(
                     title=fields[0],
                     url=fields[1],
@@ -54,9 +66,3 @@ class Question(models.Model):
     question_tags = models.CharField(max_length=100, default='', null=False)
     img = models.CharField(max_length=100, default='')
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE, null=True)
-
-
-class Job(models.Model):
-    name = models.CharField(max_length=100, default='', null=False)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
