c187968529eddb311c98080e56810697574f73db
cshiz <64184518+cshiz9206@users.noreply.github.com>
2024-04-18 22:44:11 +0900

    baseline search
    + required fix skill button error

--- GTProject/TopicsTrending/urls.py
+++ GTProject/TopicsTrending/urls.py
@@ -21,6 +21,7 @@ router.register(r"questions", QuestionViewSet)
 
 urlpatterns = [
     path('', index, name='index'),
+    path('rest/', include(router.urls)),
     path('TopicsTrending/skill-list/<str:job>/', skill_list, name='skill_list'),
     path('TopicsTrending/info-list/<str:skill>/', info_list, name='info_list'),
     path('TopicsTrending/job-list-by-skill/<str:skill>', job_list_by_skill, name='job_list_by_skill'),
