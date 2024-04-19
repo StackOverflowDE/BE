09741ddd59d17b18d0c490f3f08cb1bb35165a77
cshiz <64184518+cshiz9206@users.noreply.github.com>
2024-04-19 20:28:54 +0900

    데이터 유효성 검사 2
    - sof 링크 데이터 검사 필요
    - 표지(웹 첫 화면) 수정 예정
    + 깃허브 이미지 데이터 검사 완료

--- GTProject/parser.py
+++ GTProject/parser.py
@@ -37,15 +37,7 @@ def job_skill_refactor(rows):
     for row in rows[1:]:
         categories = row[1].split(', ')
         skills = eval(row[2])
-        # for category, skill_list in zip(categories, skills):
-        #     category_dict = result.get(category, {})
-        #     for skill in skills:
-        #         category_dict[skill] = category_dict.get(skill, 0) + 1
-        #
-        #     result[category] = category_dict
-        #
-        # for category in result:
-        #     result[category] = dict(sorted(result[category].items(), key=lambda x: x[1], reverse=True))
+
         for category in categories:
             if category not in result:
                 result[category] = []
@@ -70,46 +62,31 @@ def job_skill_DB_loader(job_skill):
 
 def git_repo_parser():
     # CSV 파일 경로
-    file_path = os.path.join(data_path, 'git')
-    git_img_path = os.path.join(img_path, 'git')
-    for skill in os.listdir(file_path):
-        # git CSV 파일 먼저 파싱
-        skill_data_path = os.path.join(file_path, skill)
-        for csv_file in os.listdir(skill_data_path):
-            csv_path = os.path.join(skill_data_path, csv_file)
-            with open(csv_path, newline='', encoding='utf-8') as csvfile:
-                # CSV 파일을 읽어들이는데 사용할 reader 생성
-                reader = csv.reader(csvfile)
-                # reader 객체를 subscriptable하게 만들기 위해 각 행을 리스트로 저장
-                rows = [row for row in reader]
-                rows = rows[1:]
-                rows = sorted(rows, key=lambda x: x[6].lower())
+    file_paths = os.path.join(data_path, 'git')
+    for skill in os.listdir(file_paths):
+        csv_path = os.path.join(file_paths, skill)
+        # git CSV 파일 파싱
+        with open(csv_path, newline='', encoding='utf-8') as csvfile:
+            # CSV 파일을 읽어들이는데 사용할 reader 생성
+            reader = csv.reader(csvfile)
+            # reader 객체를 subscriptable하게 만들기 위해 각 행을 리스트로 저장
+            rows = [row for row in reader]
+            rows = rows[1:]
 
-            # git 이미지 파일 파싱
-            skill_img_path = os.path.join(git_img_path, skill)
+        # skill 값 추출
+        skill = skill[9:]
+        skill = skill[:-4]
 
-            if csv_path[-9:] == 'forks.csv':
-                git_img_file_path = os.path.join(skill_img_path, 'forks')
-                for i, img in enumerate(os.listdir(git_img_file_path)):
-                    if len(os.listdir(git_img_file_path)) <= len(rows):
-                        rows[i].append(os.path.join(git_img_file_path, img))
-            elif csv_path[-9:] == 'stars.csv':
-                git_img_file_path = os.path.join(skill_img_path, 'stars')
-                for i, img in enumerate(os.listdir(git_img_file_path)):
-                    if len(os.listdir(git_img_file_path)) <= len(rows): # 레포 수 보다 이미지가 많으면 예외처리
-                        rows[i].append(os.path.join(git_img_file_path, img))
-
-            git_repo_DB_loader(skill, rows)
+        git_repo_DB_loader(skill, rows)
 
 
 def git_repo_DB_loader(skill, repo_infos):
     for info in repo_infos:
-        print(info)
+        # print(info)
         tech_stack = Skill.objects.filter(name=skill).first()
         formatted_date_string = info[4].replace(',', '')
         formatted_date_string = formatted_date_string.replace('GMT+9', '+0900')
         date_time_obj = datetime.strptime(formatted_date_string, '%b %d %Y %I:%M %p %z')
-
         try:
             Repository.objects.create(skill=tech_stack,
                                       repo_title=info[0],
@@ -126,32 +103,21 @@ def git_repo_DB_loader(skill, repo_infos):
 def sof_question_parser():
     # CSV 파일 경로
     file_path = os.path.join(data_path, 'sof')
-    sof_img_path = os.path.join(img_path, 'sof')
     for skill in os.listdir(file_path):
-        # git CSV 파일 먼저 파싱
+        # sof CSV 파일 파싱
         csv_path = os.path.join(file_path, skill)
-        #
-
         with open(csv_path, newline='', encoding='utf-8') as csvfile:
             # CSV 파일을 읽어들이는데 사용할 reader 생성
             reader = csv.reader(csvfile)
             # reader 객체를 subscriptable하게 만들기 위해 각 행을 리스트로 저장
             rows = [row for row in reader]
+            rows = rows[1:]
 
-        rows = rows[1:]
-        rows = sorted(rows, key=lambda x: x[3].lower())
-
-        # git 이미지 파일 파싱
+        # skill 값 추출
         skill = skill[9:]
         skill = skill[:-4]
 
-        skill_img_path = os.path.join(sof_img_path, skill)
-
-        for i, img in enumerate(os.listdir(skill_img_path)):
-            if len(os.listdir(skill_img_path)) <= len(rows):
-                rows[i].append(os.path.join(skill_img_path, img))
-
-        sof_question_DB_loader(skill, rows[1:])
+        sof_question_DB_loader(skill, rows)
 
 
 def sof_question_DB_loader(skill, question_info):
@@ -160,9 +126,9 @@ def sof_question_DB_loader(skill, question_info):
         print(info)
         tech_stack = Skill.objects.filter(name=skill).first()
 
-        if info[5][-1] == 'k':
+        if info[5][-1] == 'k':  # 킬로 단위 처리
             view = str(int(info[5][:-1]) * 1000)
-        elif info[5][-1] == 'm':
+        elif info[5][-1] == 'm':  # 메가 단위 처리
             view = str(int(float(info[5][:-1]) * 1_000_000))
         else:
             view = info[5]
